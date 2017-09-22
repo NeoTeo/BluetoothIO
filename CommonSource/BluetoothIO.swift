08d8c3f344ceeded5642814a5ee242886de8fb0b
Teo Sartori <m@sartori.dk>
2017-09-22 13:16:43 +0200

    Make shared instance public.

--- CommonSource/BluetoothIO.swift
+++ CommonSource/BluetoothIO.swift
@@ -52,7 +52,7 @@ class BluetoothIO : NSObject {
     var characteristicsForService: [CBUUID : [CBUUID]]!
     var handlerForCharacteristic: [CBUUID : (CBCharacteristic) throws -> Void]!
     
-    static let sharedInstance : BluetoothIO = {
+    public static let sharedInstance : BluetoothIO = {
         
         let instance = BluetoothIO()
         return instance
